263547af12e4c4462cad47bf10af593736078954
Simon Bugert <simon.bugert@gmail.com>
2018-10-14 21:39:30 +0000

    WIP

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as program from 'commander';
-import * as inquirer from 'inquirer';
+// import * as inquirer from 'inquirer';
+import { copySync, existsSync, realpathSync } from 'fs-extra';
+import { resolve } from 'path';
+
+const appDirectory = realpathSync(process.cwd());
+const resolveApp = (relativePath: string) =>
+  resolve(appDirectory, relativePath);
+const resolveOwn = (relativePath: string) =>
+  resolve(__dirname, '../..', relativePath);
 
 // TODO
 // bootstrap:
@@ -12,7 +20,27 @@ import * as inquirer from 'inquirer';
 //   - Add name/slug objects to .runtimeconfig.json
 // Save .runtimeconfig.json
 
-const bootstrap = () => {
+const checkFile = (filename: string) => {
+  if (existsSync(resolveApp(filename))) {
+    console.log(filename + ' exists');
+    // Do something
+  } else {
+    console.error(filename + ' is missing');
+    // Do something
+  }
+};
+
+console.log(resolveOwn('template'));
+
+const bootstrap = (foldername: string): void => {
+  console.log('Bootstrapping into:', resolveApp(foldername));
+
+  if (existsSync(resolveApp(foldername))) {
+    console.log('folder exists');
+    return;
+  }
+
+  copySync(resolveOwn('template'), resolveApp(foldername));
   // ask for the name of the new project
   // create the following files from templates:
   // - package.json
@@ -21,18 +49,19 @@ const bootstrap = () => {
   // - database.rules.json
   // - firestore.indexes.json
   // - storage.rules
+  //
   console.log('  - now bootstrapping 👢 🎀');
 };
 
-const importIntoDB = (): void => {
-  // import content (articles/pages)
-  console.log('  - now importing');
-};
-
-const exportFromDB = (): void => {
-  // export content (articles/pages)
-  console.log('  - now exporting');
-};
+// const importIntoDB = (): void => {
+//   // import content (articles/pages)
+//   console.log('  - now importing');
+// };
+//
+// const exportFromDB = (): void => {
+//   // export content (articles/pages)
+//   console.log('  - now exporting');
+// };
 
 const configure = () => {
   // check if this folder contains an already bootstrapped project
@@ -41,21 +70,76 @@ const configure = () => {
   // ask if config should be modified
   // walk config tree and ask questions
   console.log('  - now configuring 👩🏿‍🎓');
+  checkFile('package.json');
+  checkFile('firebase.json');
+  checkFile('firestore.rules');
+  checkFile('firestore.indexes.json');
+  checkFile('storage.rules');
+  checkFile('.firebaserc');
 };
 
+program.version('0.0.0', '-v, --version');
+
 program
-  .version('0.0.0', '-v, --version')
-  .option('-b, --bootstrap', 'bootstrap project')
-  .option('-c, --configure', 'configure project')
-  .option('-i, --import', 'import data into database')
-  .option('-e, --export', 'export data form database')
-  .parse(process.argv);
+  .command('bootstrap [destination]')
+  .description('bootstrap project')
+  .action(bootstrap);
+
+program
+  .command('configure')
+  .description('configure project')
+  .action(configure);
+// .command('-c, --configure', 'configure project')
+// .command('-i, --import', 'import data into database')
+// .command('-e, --export', 'export data form database')
+
+program.parse(process.argv);
 
 console.log("Gettin' spooky 👻  with poltergeist:");
-if (program.bootstrap) bootstrap();
-if (program.configure) configure();
-if (program.import) importIntoDB();
-if (program.export) exportFromDB();
+// program
+//   .version('0.0.1')
+//   .option('-C, --chdir <path>', 'change the working directory')
+//   .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+//   .option('-T, --no-tests', 'ignore test hook');
+//
+// program
+//   .command('setup')
+//   .description('run remote setup commands')
+//   .action(function() {
+//     console.log('setup');
+//   });
+//
+// program
+//   .command('exec <cmd>')
+//   .description('run the given remote command')
+//   .action(function(cmd) {
+//     console.log('exec "%s"', cmd);
+//   });
+//
+// program
+//   .command('teardown <dir> [otherDirs...]')
+//   .description('run teardown commands')
+//   .action(function(dir, otherDirs) {
+//     console.log('dir "%s"', dir);
+//     if (otherDirs) {
+//       otherDirs.forEach(function(oDir: any) {
+//         console.log('dir "%s"', oDir);
+//       });
+//     }
+//   });
+//
+// program
+//   .command('*')
+//   .description('deploy the given env')
+//   .action(function(env) {
+//     console.log('deploying "%s"', env);
+//   });
+//
+// program.parse(process.argv);
+// if (program.bootstrap) bootstrap(destination);
+// if (program.configure) configure();
+// if (program.import) importIntoDB();
+// if (program.export) exportFromDB();
 
 // const OUTPUT = require('./config.json');
 
